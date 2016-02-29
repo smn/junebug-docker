9ea936f33a25621bc80dba4183723e19fdfb6c24
Simon de Haan <simon@praekeltfoundation.org>
2016-02-29 20:56:05 +0200

    add some debug info on what Junebug is connecting to

--- junebug-entrypoint.sh
+++ junebug-entrypoint.sh
@@ -1,11 +1,22 @@
 #!/bin/bash -e
 
+REDIS_HOST=${REDIS_HOST:-127.0.01}
+REDIS_PORT=${REDIS_PORT:-6379}
+REDIS_DB=${REDIS_DB:-1}
+AMQP_HOST=${AMQP_HOST:-127.0.0.1}
+AMQP_VHOST=${AMQP_VHOST:-/guest}
+AMQP_PORT=${AMQP_PORT:-5672}
+AMQP_USER=${AMQP_USER:-guest}
+AMQP_PASSWORD=${AMQP_PASSWORD:-guest}
+
+echo "Starting Junebug with redis://$REDIS_HOST:$REDIS_PORT/$REDIS_DB and amqp://$AMQP_USER:$AMQP_PASSWORD@$AMQP_HOST:$AMQP_PORT/$AMQP_VHOST"
+
 jb --port 8080 \
-    --redis-host ${REDIS_HOST:-127.0.01} \
-    --redis-port ${REDIS_PORT:-6379} \
-    --redis-db ${REDIS_DB:-1} \
-    --amqp-host ${AMQP_HOST:-127.0.0.1} \
-    --amqp-vhost ${AMQP_VHOST:-/guest} \
-    --amqp-port ${AMQP_PORT:-5672} \
-    --amqp-user ${AMQP_USER:-guest} \
-    --amqp-password ${AMQP_PASSWORD:-guest}
+    --redis-host $REDIS_HOST \
+    --redis-port $REDIS_PORT \
+    --redis-db $REDIS_DB \
+    --amqp-host $AMQP_HOST \
+    --amqp-vhost $AMQP_VHOST \
+    --amqp-port $AMQP_PORT \
+    --amqp-user $AMQP_USER \
+    --amqp-password $AMQP_PASSWORD
